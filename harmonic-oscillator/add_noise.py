461d24c71e9c5e93eb4eab81cda672db1dda4da1
Aaron Alphonsus <aaron.alphonsus@gmail.com>
2018-07-10 10:34:42 -0400

    Saved data to netCDF file. TODO: Read file

--- harmonic-oscillator/add_noise.py
+++ harmonic-oscillator/add_noise.py
@@ -1,9 +1,13 @@
 # Sample position values of harmonic oscillator at various times and add 
-# gaussian noise
+# gaussian noise. Save data to netCDF file.
 
-import random
+import json
 import math
 import matplotlib.pyplot as plt
+import netCDF4
+import numpy as np
+
+import random
 
 def add_noise(x0, interval, maxtime, springconst, mass): 
     # Fill time and position vectors beginning at 0.0 and taking observations at
@@ -21,13 +25,57 @@ def add_noise(x0, interval, maxtime, springconst, mass):
 
         t += interval 
 
-    # print(pos)
+    # Write data to netCDF file format
+    timenp = np.asarray(time)
+    posnp = np.asarray(pos)
+    noisydatanp = np.asarray(noisydata)
+
+    ncfile = netCDF4.Dataset('data.nc', mode='w', format='NETCDF4_CLASSIC')
+    # print(ncfile)
+    time_dim = ncfile.createDimension('time', None)
+    pos_dim = ncfile.createDimension('pos', 21)
+    noisydata_dim = ncfile.createDimension('noisydata', 21)
+    # for dim in ncfile.dimensions.items():
+    #     print(dim)
+    ncfile.title = 'Noisy Data'
+    
+    timecdf = ncfile.createVariable('time', np.float32, ('time',))
+    timecdf[:] = timenp
+    poscdf = ncfile.createVariable('pos', np.float32, ('pos',))
+    poscdf[:] = posnp
+    noisydatacdf = ncfile.createVariable('noisydata', np.float32, 
+            ('noisydata',))
+    noisydatacdf[:] = noisydatanp
+    
+    # print(timecdf)
+    # print(poscdf)
+    # print(noisydatacdf)
+   
+    ncfile.close()
+
+
+    # print(timenp)
     # print(time)
 
-    # Plot position (red circles) and noisy position data (blue stars) wrt time
-    fig, axs = plt.subplots(nrows = 1, ncols = 1)
-    axs.plot(time, pos, 'ro')
-    axs.plot(time, noisydata, 'b*')
-    plt.show()
+    # h5f = h5py.File('data.h5', 'w')
+    # h5f.create_dataset('dataset_1', data=pos)
+    # h5f.close()
+
+    # # Write data to file using json
+    # data = {'time'      : time,
+    #         'pos'       : pos,
+    #         'noisydata' : noisydata}
+    # with open('data.txt', 'w') as f:
+    #     json.dump(data, f, ensure_ascii=False)
+
+    # print(time)
+    # print(pos)
+    # print(noisydata)
+
+    # # Plot position (red circles) and noisy position data (blue stars) wrt time
+    # fig, axs = plt.subplots(nrows = 1, ncols = 1)
+    # axs.plot(time, pos, 'ro')
+    # axs.plot(time, noisydata, 'b*')
+    # plt.show()
 
 add_noise(1.0, 0.5, 10, 1.0, 1.0)
