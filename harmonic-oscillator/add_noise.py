4da7fdc43e58b57a8c37f462a449ff58ded8783d
Aaron Alphonsus <aaron.alphonsus@gmail.com>
2018-07-10 09:11:51 -0400

    Changed time sampling and added gaussian noise.

--- harmonic-oscillator/add_noise.py
+++ harmonic-oscillator/add_noise.py
@@ -1,27 +1,33 @@
-# Sample position values of harmonic oscillator at various times
+# Sample position values of harmonic oscillator at various times and add 
+# gaussian noise
 
 import random
 import math
 import matplotlib.pyplot as plt
 
-def add_noise(x0, springconst, mass, obs, maxtime):
-
-    # List comprehension to fill a list with 'obs' random numbers. The 
-    # list is then sorted. Numbers are rounded to 1 decimal place.
-    time = [round(random.uniform(0, maxtime), 1) for _ in range(obs)]
-    time.sort()
-
-    # Fill the position vector
+def add_noise(x0, interval, maxtime, springconst, mass): 
+    # Fill time and position vectors beginning at 0.0 and taking observations at
+    # each interval
+    t = 0.0
+    time = []
     pos = []
-    for t in time:
-        pos.append(x0*math.cos(math.sqrt(springconst/mass)*t))
+    noisydata = []
+    while t <= maxtime:
+        time.append(t)
+
+        p = x0*math.cos(math.sqrt(springconst/mass)*t)
+        pos.append(p)
+        noisydata.append(p + random.gauss(0, 0.5)) 
+
+        t += interval 
 
     # print(pos)
     # print(time)
 
-    # Plot position wrt time as discrete points
+    # Plot position (red circles) and noisy position data (blue stars) wrt time
     fig, axs = plt.subplots(nrows = 1, ncols = 1)
     axs.plot(time, pos, 'ro')
+    axs.plot(time, noisydata, 'b*')
     plt.show()
 
-add_noise(1.0, 1.0, 1.0, 10, 20)
+add_noise(1.0, 0.5, 10, 1.0, 1.0)
