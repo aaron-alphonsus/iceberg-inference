4fa5496e920a211d2e0a206d8d688971a1791e8c
Javier <javiersalazar6@gmail.com>
2018-08-02 17:14:37 -0400

    Forward model revised

--- Forward.py
+++ Forward.py
@@ -9,20 +9,22 @@ import math # calculations
 from scipy.interpolate import interpn # for latitude/longitude grid
 import h5py # read files
 #import matplotlib.cm as cm
-#-------------parameters---------------------------------------57 43.8 0.01
-x0,y0,u0,v0 = 304, 61, 0.0, 0.0 # postion and intial velocity for iceberg
+#-------------parameters---------------------------------------308.0 55.9
+x0,y0,u0,v0 = 308.0, 55.9, 0.5, 0.5 # postion and intial velocity for iceberg
 inferValues = [1.0, 0.1] # c_water and c_air
 plotOption = 1 # 0 -> no image overlay. 1 -> image background
 resolutionData = 88
 #----------calculations-----------------------------------
-xvec = [[x0,y0]] # first value
+#xvec = [[x0,y0]] # first value
+
 def plotData(pointPairs, plotOption, errorCode):
     xData=[x[0] for x in pointPairs]
     yData=[x[1] for x in pointPairs]
     if (plotOption == 0):
         plt.plot(xData,yData)
+        #plt.axis((lon_current[0],lon_current[-1],lat_current[0],lat_current[-1]))
     if (plotOption == 1):
-        img = plt.imread("land_mass.png") # image for scatter background
+        img = plt.imread("land_mass2.png") # image for scatter background
         plt.imshow(img)
         xData2 = np.interp(xData, [lon_current[0], lon_current[-1]], [0, len(lon_current)]) # linearly map lat/lon values to image pixel locations
         yData2 = np.interp(yData, [lat_current[0], lat_current[-1]], [0, len(lat_current)])
@@ -44,21 +46,16 @@ def plotData(pointPairs, plotOption, errorCode):
     if (errorCode == 3):
         print("Program finished early. Object touched land. Nan interpolation error.")
     plt.show()
-def checkValidPoints(x,y):
-    global u_matrix_ocean
-    global lon_current
-    global lat_current
-    if (x < rangeLon[0] or x > rangeLon[1] or y < rangeLat[0] or y > rangeLat[1]):
-        global xvec
-        plotData(xvec, plotOption, 1)
-        quit()
-    lon_current = np.asarray(lon_current)
-    lat_current = np.asarray(lat_current)
-    idx = (np.abs(lon_current - x)).argmin()
-    idy = (np.abs(lat_current - y)).argmin()
-    if (math.isnan(u_matrix_ocean[0,idy,idx]) == True):
-        plotData(xvec, plotOption, 3)
-        quit()
+def checkValidPoints(x, y, values, values2):
+    if (x < values2[2][0] or x > values2[2][1] or y < values2[1][0] or y > values2[1][1]):
+        return False
+    values[6] = np.asarray(values[6])
+    values[5] = np.asarray(values[5])
+    idx = (np.abs(values[6] - x)).argmin()
+    idy = (np.abs(values[5] - y)).argmin()
+    if (math.isnan(values[1][0,idy,idx]) == True):
+        return False
+    return True
 #------------input stored data and process-----------------------
 def fixGiantMess(filename):
     hdf5file = h5py.File(filename, 'r')
@@ -82,97 +79,116 @@ def fixGiantMess(filename):
 #-----------convert meters/sec to deg/sec--------------------------------------------------
 def convertUnits(u, v, x, y): # not currently used in code
     length_v = 111320.0 # for latitude the length is fixed per degree
-    v_new = 3.6*v/length_v # veolicty is deg/sec
-    # meters per degree is a function for longitude that depends on latitude
     length_u = float(40075000.0*math.degrees(math.cos(math.radians(y)))/360.0)
-    u_new = 3.6*u/length_u # constant is related to length at equator
+    v_new = 3600*24*(v/(length_v)) # veolicty is deg/sec
+    # meters per degree is a function for longitude that depends on latitude
+    u_new = 3600*24*(u/(length_u)) # constant is related to length at equator
     #u_new = u
     #v_new = v
     return [u_new, v_new]
 #--------------coriolis force function------------------------------
-def Fcor(state, t):
+def Fcor(state, t, values, values2, time, airPoints):
     y = state[1] # y position degree latitude
-    [u_wind, v_wind] = LookUpAir(state[0], state[1], t) # wind speed
+    u_wind = airPoints[0]
+    v_wind = airPoints[1]
+    #[u_wind, v_wind] = LookUpAir(state[0], state[1], t, values, values2, time) # wind speed
     latitude_rad = (math.pi/180)*y # convert to radian
     coriolis_freq = 2*(7.2921*(10**-5))*math.sin(latitude_rad) # coriolis freq/paremeter/coefficient
     coriolisforce_permass_x = -coriolis_freq*u_wind # per equation given velocity as meters/s
     coriolisforce_permass_y = -coriolis_freq*v_wind
     return [coriolisforce_permass_x, coriolisforce_permass_y] # Force meter/s^2
 #------------current speeds using location
-def LookUpWater(state, t):
+def LookUpWater(state, t, values, values2, timeRange):
     x = state[0] # latitude/longitude info
     y = state[1]
-    checkValidPoints(x,y)
     if(t > timeRange[-1]): # ode goes over range of time by 0.01 for example so time is capped at final time
-         t = timeRange[-1]
-    u_point = interpn((timeData, lat_current , lon_current), u_matrix_ocean, np.array([t, y, x]).T) # interpolate from grid info
-    v_point = interpn((timeData, lat_current , lon_current), v_matrix_ocean, np.array([t, y, x]).T)
-    [u_convert, v_convert] = convertUnits(u_point[0], v_point[0], x, y) # convert units to deg info
-    return [u_convert,v_convert]
+        t = timeRange[-1]
+    u_point = interpn((values2[0], values[5] , values[6]), values[1], np.array([t, y, x]).T) # interpolate from grid info
+    v_point = interpn((values2[0], values[5] , values[6]), values[2], np.array([t, y, x]).T)
+    return [u_point[0],v_point[0]]
+
+    #[u_convert, v_convert] = convertUnits(u_point[0], v_point[0], x, y) # convert units to deg info
 #--------------air interpolation-------------------------
-def LookUpAir(x,y, t):
+def LookUpAir(x,y, t, values, values2, timeRange):
     if(t > timeRange[-1]): # same as look up water
          t = timeRange[-1]
-    u_point2 = interpn((timeData, lat_wind , lon_wind), u_matrix_wind, np.array([t, y, x]).T) # interpolate using linear
-    v_point2 = interpn((timeData, lat_wind , lon_wind), v_matrix_wind, np.array([t, y, x]).T)
-    [u_convert, v_convert] = convertUnits(u_point2[0], v_point2[0], x, y) # convert units
-    return [u_convert,v_convert]
+    u_point2 = interpn((values2[0], values[7] , values[8]), values[3], np.array([t, y, x]).T) # interpolate using linear
+    v_point2 = interpn((values2[0], values[7] , values[8]), values[4], np.array([t, y, x]).T)
+    #[u_convert, v_convert] = convertUnits(u_point2[0], v_point2[0], x, y) # convert units
+    return [u_point2[0],v_point2[0]]
 #----------water force computation--------------------
-def Fwater(state,c_w, t): # water force
-    u_ocean=np.array(LookUpWater(state, t)) # get from ocean currents
+def Fwater(state, c_w, t, values, values2, time): # water force
+    u_ocean=np.array(LookUpWater(state, t, values, values2, time)) # get from ocean currents
     u_berg=np.array([state[2],state[3]])
     u_relw=np.subtract(u_ocean,u_berg) # subtract iceberg velocity to get relative
     nuw=np.linalg.norm(u_relw) # l2 norm
     return c_w*nuw*u_relw
 #------------air force computation-------------------------
-def F_air(state,c_a, t):
-    u_air=np.array(LookUpAir(state[0],state[1], t)) # same as above
-    u_berg=np.array([state[2],state[3]])
-    u_rela=np.subtract(u_air,u_berg)
+def F_air(state, c_a, t, values, values2, time):
+    u_air=np.array(LookUpAir(state[0], state[1], t, values, values2, time)) # same as above
+    u_berg=np.array([state[2], state[3]])
+    u_rela=np.subtract(u_air, u_berg)
     nua=np.linalg.norm(u_rela)
-    return c_a*nua*u_rela
+    return [c_a * nua * u_rela, u_air]
 #-----------rhs ode model----------------------------
-def rhs(t, state, theta):
+def rhs(t, state, array):
+    theta = array[0]
+    values = array[1]
+    values2 = array[2]
+    time = array[3]
     x = state[0] # x position
     y = state[1] # y position
     u = state[2] # x velocity
     v = state[3] # y velocity
     c_water = theta[0] # set inference values
     c_air = theta[1]
-    Water_F=Fwater(state,theta[0], t) # get forces
-    Air_F=F_air(state,theta[1], t)
-    Cor_F=Fcor(state, t)
-    return [u,v, Water_F[0]+Air_F[0]+Cor_F[0],Water_F[1]+Air_F[1]+Cor_F[1]]
+    pointState = checkValidPoints(x,y, values, values2)
+    if (pointState == True):
+        Water_F=Fwater(state,theta[0], t, values, values2, time) # get forces
+        [Air_F, airPoints] =F_air(state,theta[1], t, values, values2, time)
+        Cor_F=Fcor(state, t, values, values2, time, airPoints)
+        return [u, v, Water_F[0]+Air_F[0]+Cor_F[0],Water_F[1]+Air_F[1]+Cor_F[1]]
+    else:
+        return None
 #-------------------forward model given paramters of inference-------------
-def ForwardModel(time, theta, state0):
-    global xvec
+def ForwardModel(time, theta, state0, values, values2):
+    xvec = [[state0[0], state0[1]]]
     solver = ode(rhs)
-    solver.set_integrator('vode', method='bdf', with_jacobian=False) #set steps, ode method, etc.. Nonstiff only
+    solver.set_integrator('dopri5') #set steps, ode method, etc.. Nonstiff only
+#    solver.set_solout(checkValidPoints)
     solver.set_initial_value(state0, time[0])
-    solver.set_f_params(theta)
+    solver.set_f_params([theta, values, values2, time])
     for t in time[1:]:
         #assert(solver.successful())
         solver.integrate(t)
         x = solver.y[0]
         y = solver.y[1]
-        checkValidPoints(x, y)
+        #checkValidPoints(x, y)
         xvec = xvec+[[x,y]]
     return xvec
+def fixSecondMess():
+    values = fixGiantMess('IcebergData.h5')
+    timeData = np.array(range(len(values[0])))
+    rangeLat = [max(values[7][0], values[5][0]), min(values[7][-1], values[5][-1])]
+    rangeLon = [max(values[8][0], values[6][0]), min(values[8][-1], values[6][-1])]
+    values2 = [timeData, rangeLat, rangeLon]
+    return [values, values2]
 #------------------global calculations used in functions-----------------------
-intialState = [x0, y0, u0, v0]
-[timeArray, u_matrix_ocean, v_matrix_ocean, u_matrix_wind, v_matrix_wind, lat_current, lon_current, lat_wind, lon_wind] = fixGiantMess('IcebergData.h5')
-timeRange = np.linspace(0,len(timeArray)-1,resolutionData)
-timeData = np.array(range(len(timeArray)))
-# print(timeRange)
-# print(timeArray)
-# print(timeData)
-#timeRange = np.array(range(0,len(timeArray)))
-rangeLat = [max(lat_wind[0], lat_current[0]), min(lat_wind[-1], lat_current[-1])]
-rangeLon = [max(lon_wind[0], lon_current[0]), min(lon_wind[-1], lon_current[-1])]
+initialState = [x0, y0, u0, v0]
+#[timeArray, u_matrix_ocean, v_matrix_ocean, u_matrix_wind, v_matrix_wind, lat_current, lon_current, lat_wind, lon_wind] = fixGiantMess('IcebergData.h5')
+#values = fixGiantMess('IcebergData.h5')
+#timeRange = np.linspace(0,len(values[0])-1,resolutionData)
+#timeData = np.array(range(len(values[0])))
+#rangeLat = [max(lat_wind[0], lat_current[0]), min(lat_wind[-1], lat_current[-1])]
+#rangeLon = [max(lon_wind[0], lon_current[0]), min(lon_wind[-1], lon_current[-1])]
+#values2 = [timeData, rangeLat, rangeLon]
+[values, values2] = fixSecondMess()
+timeRange = np.linspace(0,len(values[0])-1,200)
+print(len(values[0])-1)
 #-------------main function---------------------------------
-ObsData=ForwardModel(timeRange,inferValues,intialState)
+ObsData = ForwardModel(timeRange,inferValues,initialState, values, values2)
 #print(ObsData)
 # matrix = u_matrix_ocean[0,:,:]
 # matrixLand = np.isnan(matrix)
 # plt.imsave('land_mass2.png', np.array(~matrixLand), cmap=cm.gray)
-plotData(ObsData, plotOption, 0)
+plotData(ObsData, 0, 0)
--- predictiveHarmonic/predictiveModel.py
+++ predictiveHarmonic/predictiveModel.py
@@ -7,71 +7,38 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from math import *
 from MakeFigure import *
-#from ForwardModel import *
+from IcebergForward import *
 from scipy.integrate import ode
 #----------read data--------------------------------
-filename = 'data_mcmc.h5'
+filename = 'AaronData.h5'
 hdf5file = h5py.File(filename, 'r')
 theta_1 = hdf5file['data_mcmc/theta1'].value
 theta_2 = hdf5file['data_mcmc/theta2'].value
-#-----------forward model code------------------------
-def rhs(t, state, theta):
-    x = state[0]
-    u = state[1]
-
-    omega = theta[0]
-    gamma = theta[1]
-
-    return [u, -omega*omega*x-gamma*u]
-
-def jacobian(t, state, theta):
-    omega = theta[0]
-    gamma = theta[1]
-
-    return [[0.0, 1.0],[-omega*omega, -2.0*gamma*omega]]
-
-# time - times to observe numerical solution of the damped harmonic oscilator
-# theta - parameters: [\omega, \gamma]
-def ForwardModel(time, theta, state0):
-    # create a solver
-    solver = ode(rhs, jacobian)
-
-    # set the numerical options (e.g., method and tolerances)
-    solver.set_integrator('vode', method='bdf', with_jacobian=True) # play with tolerances?
-
-    solver.set_initial_value(state0, time[0])
-    solver.set_f_params(theta)
-    solver.set_jac_params(theta)
-
-    xvec = [state0[0]]
-
-    for t in time[1:]:
-        #assert(solver.successful())
-        solver.integrate(t)
-
-        xvec = xvec+[solver.y[0]] # only need the real part
-
-    return xvec
+theta_1 = theta_1[89999:99999]
+theta_2 = theta_2[89999:99999]
 #-------------------main code------------------
-T = 10 # observation points for position
-t0 = 0.0
-tf = 10.0
-state0 = [1.0, 0.0] # inital state
-positionArray = [[0]*T]*len(theta_1)
-timeArray = [[0]*T]*len(theta_1)
+def WriteData(hdf5file, name, data):
+    if name in hdf5file:
+        del hdf5file[name]
+    hdf5file.create_dataset(name, data=data)
+state0 = [0, -1, 0, 0] # inital state
+timeArray = list(range(100))
+T = len(timeArray)
+positionArray = [[0, 0]*T]*len(theta_1)
 for i in range(0, len(theta_1)):
-    timeArray[i] = sorted(np.random.uniform(t0, tf, size=T))
-    positionArray[i] = ForwardModel(timeArray[i], [theta_1[i], theta_2[i]], state0)
-timePlot = np.hstack(timeArray)
-positionPlot = np.hstack(positionArray)
-# plt.hist(positionPlot, bins=100)
-# plt.show()
-#print(positionPlot)
+    positionArray[i] = ForwardModel(timeArray, [theta_1[i], theta_2[i]], state0)
+    print(i)
+positionArray = np.array(positionArray)
+xData = np.hstack(positionArray[:,:,0])
+yData = np.hstack(positionArray[:,:,1])
+hdf5file2 = h5py.File('predictiveValues.h5', 'a')
+WriteData(hdf5file2, 'data/predictiveX', xData)
+WriteData(hdf5file2, 'data/predictiveY', yData)
 fig = MakeFigure(450, 1)
 ax = plt.gca()
-ax.set_title('2D Histogram', fontsize = 12)
-ax.set_xlabel('Time (s)', fontsize = 10)
-ax.set_ylabel('Position (m)', fontsize = 10)
-hist = ax.hist2d(timePlot, positionPlot, normed=True, bins = (25,25), cmap = plt.cm.viridis)
+ax.set_title('Iceberg Predictive Model', fontsize = 12)
+ax.set_xlabel('Latitude (deg)', fontsize = 10)
+ax.set_ylabel('Longitude (deg)', fontsize = 10)
+hist = ax.hist2d(xData, yData, normed=True, bins = (400,400), cmax= 0.2, cmap = plt.cm.viridis)
 plt.colorbar(hist[3], ax=ax)
 plt.show()
